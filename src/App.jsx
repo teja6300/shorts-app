4ae328a6444ece6b1ebf30c5e516420401e20287
teja6300 <218w1a1239@vrsec.ac.in>
2024-07-24 20:04:49 +0530

    shorts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Shorts from "./components/Shorts";
+
+const App = () => {
+  return (
+    <>
+      <Shorts />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/DataProvider.jsx
@@ -0,0 +1,46 @@
+const videoData = [
+  {
+    src: "https://d1mq5dy0cocnal.cloudfront.net/assets/1276861b-96ed-4618-a774-02e554987342/MP4/master.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+  {
+    src: "https://d1mq5dy0cocnal.cloudfront.net/assets/3e24d510-5b5d-4746-9340-2061964ca318/MP4/master.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+  {
+    src: "https://d1mq5dy0cocnal.cloudfront.net/assets/8854f3e7-43ad-46d5-9eb7-9ebf9c819a4d/MP4/master.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+  {
+    src: "https://d1mq5dy0cocnal.cloudfront.net/assets/c08a5097-71db-4942-957a-1a7a0df34c49/MP4/master.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+  {
+    src: "https://filesamples.com/samples/video/mp4/sample_960x540.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+  {
+    src: "https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_1mb.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+  {
+    src: "https://samplelib.com/lib/preview/mp4/sample-5s.mp4",
+    title: "ABN News",
+    desc: "Never gonna give you up Never gonna let you down Never gonna run around and desert you.",
+    tags: ["abn", "abnnews", "ABN"],
+  },
+];
+
+export default videoData;
